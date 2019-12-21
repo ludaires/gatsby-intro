52afab5493a1127630f3deaab28191c83cfadb68
Ludmilla Aires Negri <ludaires@gmail.com>
2019-12-21 17:27:35 -0500

    create simple about me page.

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+    <>
+        <h1>About me</h1>
+        <p>This is my personal site!</p>
+        <Link to='/'>&larr; back to home</Link>
+    </>
+)
